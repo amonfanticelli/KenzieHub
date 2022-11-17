b5f132054698eef8a9ac654224e147ea04b980aa
amonfanticelli <amon_fanticelli@hotmail.com>
2022-11-17 11:33:59 -0300

    refactor: improve app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Routes from "./routes";
-import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserProvider } from "./contexts/UserContext";
